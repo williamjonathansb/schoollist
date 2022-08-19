314fc0dab2916a06ebe595f03b493a3a8bbbde0a
William Jonathan <willyeager01@gmail.com>
2022-08-19 17:42:41 -0300

    feat(back): students query added in studentResolver for find students

--- schoollist-back/src/resolvers/studentResolver.ts
+++ schoollist-back/src/resolvers/studentResolver.ts
@@ -1,10 +1,23 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Query, Resolver } from "type-graphql";
+import { TypeOrmDatabase } from "../database";
 import { Student } from "../entities/student";
 
 @Resolver((of) => Student)
 export class StudentResolver {
+  private readonly studentsRepository = TypeOrmDatabase.getRepository(Student);
+
   @Query((returns) => [Student], { nullable: true })
-  async GetStudents() {
-    return null;
+  async students(
+    @Arg("cpf", { nullable: true }) cpf: string,
+    @Arg("name", { nullable: true }) name: string,
+    @Arg("email", { nullable: true }) email: string
+  ) {
+    return this.studentsRepository.find({
+      where: {
+        cpf,
+        name,
+        email,
+      },
+    });
   }
 }
