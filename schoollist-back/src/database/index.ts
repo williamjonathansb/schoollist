04db752812326e4f5c9e9409c54bb5da73c890bf
William Jonathan <willyeager01@gmail.com>
2022-08-20 17:16:06 -0300

    chore(back): configuration of database moved to enviroment variavels

--- schoollist-back/src/database/index.ts
+++ schoollist-back/src/database/index.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
 import { Student } from "../entities/student";
+import "dotenv/config";
 
 export const TypeOrmDatabase = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.POSTGRES_HOST,
   port: 5432,
-  username: "postgres",
-  password: "psql",
-  database: "opencircle",
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: true,
   logging: false,
   entities: [Student],
