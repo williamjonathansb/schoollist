c0bb7abfd2574eef20a8b8c4d03d22c4be77546b
William Jonathan <willyeager01@gmail.com>
2022-08-19 22:47:46 -0300

    chore: configuration about style for better design

---
+++ schoollist-front/src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: "#FFF",
+
+  "grey-100": "#E1E1E6",
+  "grey-300": "#C4C4CC",
+  "grey-400": "#8D8D99",
+  "grey-500": "#7C7C8A",
+  "grey-600": "#323238",
+  "grey-700": "#29292E",
+  "grey-800": "#202024",
+  "grey-900": "#121214",
+
+  "green-300": "#00e88f",
+  "green-500": "#00b871",
+  "green-700": "#008f58",
+
+  "red-500": "#AB222E",
+  "red-700": "#7A1921",
+
+  "yellow-500": "#FBA94C",
+};
