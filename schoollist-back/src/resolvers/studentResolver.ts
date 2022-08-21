2192cb54a23519da9006c0b17d440a4a86931bb9
William Jonathan <willyeager01@gmail.com>
2022-08-21 12:17:49 -0300

    feat(createstudent): created a mutation of creating of the user

--- schoollist-back/src/resolvers/studentResolver.ts
+++ schoollist-back/src/resolvers/studentResolver.ts
@@ -1,7 +1,9 @@
-import { Arg, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { TypeOrmDatabase } from "../database";
 import { Student } from "../entities/student";
 import { Like } from "typeorm";
+import { GraphQLError } from "graphql";
+import { validate } from "class-validator";
 
 @Resolver((of) => Student)
 export class StudentResolver {
@@ -18,5 +20,37 @@ export class StudentResolver {
       name: name ? Like(`%${name}%`) : name,
       email: email ? Like(`%${email}%`) : email,
     });
+  }
+
+  @Mutation((returns) => Student)
+  async createStudent(
+    @Arg("cpf") cpf: string,
+    @Arg("name") name: string,
+    @Arg("email") email: string
+  ): Promise<Student> {
+    const studentAlreadyExists = await this.studentsRepository.find({
+      where: {
+        cpf,
+      },
+    });
+
+    if (studentAlreadyExists.length > 0) {
+      throw new GraphQLError("Student already exists");
+    }
+
+    const student = this.studentsRepository.create({
+      cpf,
+      name,
+      email,
+    });
+
+    const errors = await validate(student);
+    if (errors.length > 0) {
+      throw new GraphQLError(`${errors[0].property} is in an invalid format`);
+    }
+
+    await this.studentsRepository.save(student);
+
+    return student;
   }
 }
